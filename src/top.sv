d1b7a402477ee5f7ed86622df0a7a02a3349b31e
Harshitha Naik <harshitharn2004@gmail.com>
2026-08-07 18:52:13 +0530

    Add top module with ALU design and test configuration

---
+++ src/top.sv
@@ -0,0 +1,29 @@
+`include "test_pkg.sv"
+`include "alu_if.sv"
+`include "alu_design.sv"
+module top;
+import uvm_pkg::*;
+import test_pkg::*;
+bit clk;
+bit rst;
+alu_if itf(clk,rst);
+ALU_DESIGN duv (.OPA(itf.opa), .OPB(itf.opb), .CLK(itf.clk), .RST(itf.rst), .CE(itf.ce), .MODE(itf.mode), .CIN(itf.cin), .CMD(itf.cmd), .INP_VALID(itf.inp_valid), .RES(itf.res), 
+.COUT(itf.cout), .L(itf.l), .E(itf.e), .G(itf.g), .OFLOW(itf.oflow), .ERR(itf.err));
+
+initial begin
+	uvm_config_db #(virtual alu_if)::set(null, "*", "alu_if", itf);
+	run_test("alu_test");
+end
+
+initial begin
+	clk=1'b0;
+	forever 
+	#5 clk=~clk;
+end
+
+initial begin
+    rst = 1;
+    #10;
+    rst = 0;
+end
+endmodule
