4c789d33cab8a23a62e2e8590c5c0f8570ca51ad
Harshitha Naik <harshitharn2004@gmail.com>
2026-08-07 18:51:46 +0530

    Add test_pkg package with UVM includes

---
+++ src/test_pkg.sv
@@ -0,0 +1,18 @@
+package test_pkg;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+	
+	`include "alu_seq_item.sv"
+	`include "alu_cfg.sv"
+	`include "alu_driver.sv"
+	`include "alu_act_monitor.sv"
+	`include "alu_seqr.sv"
+	`include "alu_act_agent.sv"
+	`include "alu_pas_monitor.sv"
+	`include "alu_pas_agent.sv"
+	`include "alu_scoreboard.sv"
+	`include "alu_sub.sv"
+	`include "alu_env.sv"
+	`include "alu_seq.sv"
+	`include "alu_test.sv"
+endpackage
