d54b286733ce94a28a451ab854069a8f5c232c46
Harshitha Naik <harshitharn2004@gmail.com>
2026-08-31 15:17:41 +0530

    Top Module

--- src/top.sv
+++ src/top.sv
@@ -1,6 +1,7 @@
 `include "test_pkg.sv"
 `include "alu_if.sv"
 `include "alu_design.sv"
+`include "alu_assertion.sv"
 module top;
 import uvm_pkg::*;
 import test_pkg::*;
@@ -10,9 +11,11 @@ alu_if itf(clk,rst);
 ALU_DESIGN duv (.OPA(itf.opa), .OPB(itf.opb), .CLK(itf.clk), .RST(itf.rst), .CE(itf.ce), .MODE(itf.mode), .CIN(itf.cin), .CMD(itf.cmd), .INP_VALID(itf.inp_valid), .RES(itf.res), 
 .COUT(itf.cout), .L(itf.l), .E(itf.e), .G(itf.g), .OFLOW(itf.oflow), .ERR(itf.err));
 
+alu_assertion sva (.clk(itf.clk), .rst(itf.rst), .mode(itf.mode), .cmd(itf.cmd), .inp_valid(itf.inp_valid), .err(itf.err));
+
 initial begin
 	uvm_config_db #(virtual alu_if)::set(null, "*", "alu_if", itf);
-	run_test();
+	run_test("reg_test");
 end
 
 initial begin
@@ -22,6 +25,10 @@ initial begin
 end
 
 initial begin
+    rst = 1;
+    #10;
+    rst = 0;
+    #10;
     rst = 1;
     #10;
     rst = 0;
