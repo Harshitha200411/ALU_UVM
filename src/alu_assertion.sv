ca7a389823177c3b50eec75e46e87c541b03adb0
Harshitha Naik <harshitharn2004@gmail.com>
2026-08-31 15:16:53 +0530

    ALU_Assertions

--- src/alu_assertion.sv
+++ src/alu_assertion.sv
@@ -7,49 +7,64 @@ module alu_assertion (
     input logic [1:0] inp_valid
 );
 
-    assert property (
+     assert property (
         @(posedge clk) disable iff (rst)
-        (inp_valid == 2'b01 && mode &&
-         (cmd inside {0,1,2,3,8,9,10}))
-        |-> ##[1:$](!(inp_valid inside {2'b10,2'b11})[*16] |=> err)
+        (inp_valid == 2'b01 &&
+         mode &&
+         (cmd inside {4'd0,4'd1,4'd2,4'd3,4'd8,4'd9,4'd10}))
+        ##1
+        (!(inp_valid inside {2'b10,2'b11}))[*16]
+        |=> err
     );
+
 
     assert property (
         @(posedge clk) disable iff (rst)
-        (inp_valid == 2'b10 && mode &&
-         (cmd inside {0,1,2,3,8,9,10}))
-        |-> (!(inp_valid inside {2'b01,2'b11})[*16] |=> err)
+        (inp_valid == 2'b10 &&
+         mode &&
+         (cmd inside {4'd0,4'd1,4'd2,4'd3,4'd8,4'd9,4'd10}))
+        ##1
+        (!(inp_valid inside {2'b01,2'b11}))[*16]
+        |=> err
     );
+
 
     assert property (
         @(posedge clk) disable iff (rst)
-        (inp_valid == 2'b01 && !mode &&
-         (cmd inside {0,1,2,3,4,5,12,13}))
-        |-> (!(inp_valid inside {2'b10,2'b11})[*16] |=> err)
+        (inp_valid == 2'b01 &&
+         !mode &&
+         (cmd inside {4'd0,4'd1,4'd2,4'd3,4'd4,4'd5,4'd12,4'd13}))
+        ##1
+        (!(inp_valid inside {2'b10,2'b11}))[*16]
+        |=> err
     );
+
 
     assert property (
         @(posedge clk) disable iff (rst)
-        (inp_valid == 2'b10 && !mode &&
-         (cmd inside {0,1,2,3,4,5,12,13}))
-        |-> (!(inp_valid inside {2'b01,2'b11})[*16] |=> err)
+        (inp_valid == 2'b10 &&
+         !mode &&
+         (cmd inside {4'd0,4'd1,4'd2,4'd3,4'd4,4'd5,4'd12,4'd13}))
+        ##1
+        (!(inp_valid inside {2'b01,2'b11}))[*16]
+        |=> err
     );
-    
+
     assert property (
     @(posedge clk) disable iff (rst)
     (!mode && !(cmd inside {14,15}))
-    |-> err
+    |=> err
     );
     
     assert property (
     @(posedge clk) disable iff (rst)
     (mode && !(cmd inside {11,12,13,14,15}))
-    |-> err
+    |=> err
     );
     
     assert property (
     @(posedge clk)
-    rst |-> ( !err && inp_valid == 2'b00 && cmd == 4'b0000 && !mode)
+    rst |=> ( !err && inp_valid == 2'b00 && cmd == 4'b0000 && !mode)
     );
 
 endmodule
